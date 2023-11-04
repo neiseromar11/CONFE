baa5e244847c365c605c9cdcc716781073466c81
Estudiante <Estudiante@DESKTOP-2HONTLP>
2023-11-04 12:10:05 -0500

    Add project files.

---
+++ Biblioteca_Clase/Bibliotecas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Clase
+{
+    public class Bibliotecas
+    {
+
+        public static void PIe_Pag(float[] velocidadEnKilometros, float velocidadMinima, float velocidadMaxima)
+        {
+            Console.WriteLine("\nVelocidades que superan los 100 km/h:");
+            for (int i = 0; i < velocidadEnKilometros.Length; i++)
+            {
+                if (velocidadEnKilometros[i] > 100f)
+                {
+                    Console.WriteLine(velocidadEnKilometros[i]);
+                }
+            }
+
+            Console.WriteLine($"\nVelocidad mínima en kilómetros: {velocidadMinima:F2}");
+            Console.WriteLine($"Velocidad máxima en kilómetros: {velocidadMaxima:F2}");
+        }
+
+        public static void CabeceraPantalla(float[] velocidadEnMillas, float[] frecuencia, float[] velocidadEnKilometros)
+        {
+            Console.WriteLine("VELOCIDAD EN MILLAS   FRECUENCIA   VELOCIDAD EN KILOMETROS");
+            for (int i = 0; i < velocidadEnMillas.Length; i++)
+            {
+                velocidadEnKilometros[i] = velocidadEnMillas[i] * 1.60934f;
+                Console.WriteLine($"{velocidadEnMillas[i],11:F2} {frecuencia[i],18:F2} {velocidadEnKilometros[i],18:F2}");
+            }
+        }
+    }
+}
---
+++ CONFE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca_Clase;
+namespace CONFE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] velocidadEnMillas = { 20f, 40f, 60f, 80f, 100f, 120f, 140f, 160f, 180f, 200f };
+            float[] frecuencia = { 149.34f, 298.69f, 448.03f, 597.37f, 746.71f, 896.06f, 1045.40f, 1194.74f, 1344.09f, 1493.43f };
+            float[] velocidadEnKilometros = new float[10];
+
+           Bibliotecas.CabeceraPantalla(velocidadEnMillas, frecuencia, velocidadEnKilometros);
+
+            float velocidadMinima = velocidadEnKilometros[0];
+            float velocidadMaxima = velocidadEnKilometros[0];
+
+            for (int i = 1; i < velocidadEnKilometros.Length; i++)
+            {
+                if (velocidadEnKilometros[i] < velocidadMinima)
+                {
+                    velocidadMinima = velocidadEnKilometros[i];
+                }
+                if (velocidadEnKilometros[i] > velocidadMaxima)
+                {
+                    velocidadMaxima = velocidadEnKilometros[i];
+                }
+            }
+
+            Bibliotecas.PIe_Pag(velocidadEnKilometros, velocidadMinima, velocidadMaxima);
+            Console.ReadKey();
+        }
+
+    }
+}
